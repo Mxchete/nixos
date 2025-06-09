952bdc70d7d7f9e4166f15a5d0d851f790690330
Mxchete <elliott.damato@gmail.com>
2025-06-09 15:58:51 -0400

    test

--- hyprland.nix
+++ hyprland.nix
@@ -1,34 +1,12 @@
+{ config, lib, pkgs, inputs, ... }:
 {
   programs.hyprland.enable = true; # enable Hyprland
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     # ... other packages
     pkgs.kitty # required for the default Hyprland config
   ];
 
   # Optional, hint Electron apps to use Wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, Q, exec, ghostty"
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList
-          (i:
-            let ws = i + 1;
-            in
-            [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
-  };
 }
